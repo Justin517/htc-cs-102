459873d4088a93c045d243b8675052737e4af15b
Justin517 <justin.s.wendell@gmail.com>
2019-02-27 19:27:18 -0500

    worked on movie thing

--- Week03/MovieList/MovieList/MainWindow.xaml.cs
+++ Week03/MovieList/MovieList/MainWindow.xaml.cs
@@ -33,12 +33,25 @@ namespace MovieList
 
         private void buttonAddMovie_Click(object sender, RoutedEventArgs e)
         {
+             
+            string name = textBoxTitle.Text;
+            textBoxTitle.Clear();
+            int year = Convert.ToInt32(textBoxReleaseYear.Text);
+            textBoxReleaseYear.Clear();
+            Movie newMovie = new Movie(name, year, director, );
+            MovieList.Add(newMovie);
+            MessageBox.Show(name);
+          
+            MessageBox.Show(year + "");
 
         }
 
         private void buttonShowAll_Click(object sender, RoutedEventArgs e)
         {
-
+           foreach (Movie movie in MovieList)
+            {
+                movie.ShowMovie();
+            }
         }
     }
 }
--- Week03/MovieList/MovieList/Movie.cs
+++ Week03/MovieList/MovieList/Movie.cs
@@ -5,6 +5,25 @@ namespace MovieList
 {
     public class Movie
     {
+        private string movieName;
+        private int releaseYear;
+        private string Director;
+        private int Length;
+        private string Genre;
 
+        public Movie (string Name, int ReleaseYear, string director, int length, string genre)
+        {
+            this.movieName = Name;
+            this.releaseYear = ReleaseYear;
+            this.Director = director;
+            this.Length = length;
+            this.Genre = genre;
+
+        }
+        public void ShowMovie()
+        {
+            MessageBox.Show(movieName + Environment.NewLine + releaseYear + Environment.NewLine + Director + Environment.NewLine + Length + Environment.NewLine + Genre );
+        }
+        
     }
 }
